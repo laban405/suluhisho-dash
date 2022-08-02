636f85eed2eebb68b80bce932d0a435d35f7739a
cyrilokidi <okidicyril@gmail.com>
2022-08-02 08:23:40 +0300

    Create method to send sms to service provider

---
+++ config/sms.config.js
@@ -0,0 +1,9 @@
+export const smsData = {
+  apikey: process.env.NEXT_PUBLIC_SMS_API_KEY,
+  partnerID: "3587",
+  shortcode: "SULUHISHO",
+  clientsmsid: "3587",
+  pass_type: "plain",
+};
+
+export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
