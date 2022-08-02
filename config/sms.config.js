62daa8f5f42832536e9fcb751c5e665a7eaf28fb
cyrilokidi <okidicyril@gmail.com>
2022-08-02 08:31:17 +0300

    Refractor sms request interceptor

--- config/sms.config.js
+++ config/sms.config.js
@@ -1,9 +1,3 @@
-export const smsData = {
-  apikey: process.env.NEXT_PUBLIC_SMS_API_KEY,
-  partnerID: "3587",
-  shortcode: "SULUHISHO",
-  clientsmsid: "3587",
-  pass_type: "plain",
-};
+export const apikey = process.env.NEXT_PUBLIC_SMS_API_KEY;
 
 export const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
